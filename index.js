09564062e9e5ae95d4d72cafa0f36641b546fb6d
jiongrui <jiongrui@126.com>
2020-01-06 23:25:28 +0800

    feat: add aSearch

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  * @param fmt  期望的日期格式，如： yyyy-MM-dd hh:mm:ss => 2019-11-25 12:12:30
  * @return 格式化后的日期： 2019-11-25 12:12:30
  */
-export function multiFormatDate(date = new Date(), fmt = "yyyy-MM-dd") {
+function multiFormatDate(date = new Date(), fmt = "yyyy-MM-dd") {
   if (date instanceof Date) {
   } else if (typeof date === "number") {
     date = new Date(date);
@@ -16,7 +16,7 @@ export function multiFormatDate(date = new Date(), fmt = "yyyy-MM-dd") {
 
     // 无效时间格式
     if (!isValidDate(date)) {
-      let y = M = d = h = m = s = 0;
+      let y = (M = d = h = m = s = 0);
       if (!isNaN(Number(dateStr))) {
         y = dateStr.substr(0, 4);
         M = dateStr.substr(4, 2) - 1;
@@ -39,7 +39,7 @@ export function multiFormatDate(date = new Date(), fmt = "yyyy-MM-dd") {
  * @param fmt  期望的日期格式，如： yyyy-MM-dd hh:mm:ss => 2019-11-25 12:12:30
  * @return 格式化后的日期： 2019-11-25 12:12:30
  */
-export function formatDate(date = new Date(), fmt = "yyyy-MM-dd") {
+function formatDate(date = new Date(), fmt = "yyyy-MM-dd") {
   if (!(date instanceof Date)) return;
 
   const o = {
@@ -71,7 +71,7 @@ export function formatDate(date = new Date(), fmt = "yyyy-MM-dd") {
  * @param  date
  * @return boolean true/false
  */
-export function isValidDate(date) {
+function isValidDate(date) {
   if (Object.prototype.toString.call(date) === "[object Date]") {
     // it is a date
     if (isNaN(date.getTime())) {
@@ -93,7 +93,7 @@ export function isValidDate(date) {
  * @param {*} fill 用来填充的字符串
  * @return 填充后的字符串 "123" => "00123"
  */
-export function padStart(str, len = 2, fill = "0") {
+function padStart(str, len = 2, fill = "0") {
   str = str + "";
   if (!str || len <= 0) return "";
   if (!fill || str.length >= len) return str;
@@ -106,11 +106,14 @@ export function padStart(str, len = 2, fill = "0") {
 }
 
 /**
- * A路寻址法
- * @
+ * A星寻路法
+ *
+ * @G 距离起点的步数
+ * @H 不考虑障碍，到终点的最短距离（步数）
+ * @F G + H
  */
-
-const gameMap = [
+//迷宫地图
+const MAZE = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
@@ -118,82 +121,153 @@ const gameMap = [
   [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+];
 
-export function aSearch(start, end) {
-  // let grid = new Grid(start, end, start)
-  const openList = new WeakSet()
-  const closeList = new WeakSet()
-  openList.add(start)
+function aSearch(start, end) {
+  //可到达的格子
+  const openList = new Set();
+  //已到达的格子
+  const closeList = new Set();
+  openList.add(start);
   while (openList.length > 0) {
-    const grid = openList[0]
-    findNeighbor(grid, openList)
-    grid = findMinGrid(openList)
+    //在openList中查找F值最小的节点，将其作为当前节点
+    const currentGrid = findMinGrid(openList);
+
+    //将当前节点在openList中删除
+    openList.delete(currentGrid);
+    // 将当前节点添加到closeList中
+    closeList.add(currentGrid);
+
+    const neighbors = findNeighbors(currentGrid, openList, closeList);
+
+    for (let grid of neighbors) {
+      if (!openList.has(grid)) {
+        // 临近节点不在openList，标记父节点parent、G、H、F，并放入openList
+        grid.initGrid(currentGrid, end);
+        openList.add(grid);
+      }
+    }
+
+    // 如果终点在openList中，直接返回终点
+    for (let grid of openList) {
+      if (grid.x === end.x && grid.y === end.y) {
+        return grid;
+      }
+    }
   }
+  // openList用尽，仍然找不到终点，说明终点不可达，返回空
+  return null;
 }
 
-function findNeighbor(grid, openList, closeList) {
-  if (isValidGrid(grid.x - 1, grid.y, openList, closeList)) {
-    openList.add({ x: grid.x - 1, y: grid.y })
+function findMinGrid(openList) {
+  let tempGrid = openList[0];
+  for (let grid of openList) {
+    if (grid.f < tempGrid) {
+      tempGrid = grid.f;
+    }
   }
-
-  if (isValidGrid(grid.x - 1, grid.y, openList, closeList)) {
-    openList.add({ x: grid.x - 1, y: grid.y })
-  }
-
-  if (isValidGrid(grid.x - 1, grid.y, openList, closeList)) {
-    openList.add({ x: grid.x - 1, y: grid.y })
-  }
-
-  if (isValidGrid(grid.x - 1, grid.y, openList, closeList)) {
-    openList.add({ x: grid.x - 1, y: grid.y })
-  }
+  return tempGrid;
 }
 
-function findMinGrid() {
-  return
+function findNeighbors(grid, openList, closeList) {
+  const gridList = new Set();
+  if (isValidGrid(grid.x - 1, grid.y, openList, closeList)) {
+    gridList.add(new Grid(grid.x - 1, grid.y));
+  }
+
+  if (isValidGrid(grid.x + 1, grid.y, openList, closeList)) {
+    gridList.add(new Grid(grid.x + 1, grid.y));
+  }
+
+  if (isValidGrid(grid.x, grid.y - 1, openList, closeList)) {
+    gridList.add(new Grid(grid.x - 1, grid.y));
+  }
+
+  if (isValidGrid(grid.x, grid.y + 1, openList, closeList)) {
+    gridList.add(new Grid(grid.x, grid.y + 1));
+  }
+  return gridList;
 }
 
 function isValidGrid(x, y, openList, closeList) {
-  if (x < 0 || x >= gameMap[0].length ||
-    y < 0 || y >= gameMap.length) {
-    return false
+  if (x < 0 || x >= gameMap[0].length || y < 0 || y >= gameMap.length) {
+    return false;
   }
 
-  if (openList.has({ x, y })) {
-    return false
+  if (MAZE[x][y] === 1) {
+    return false;
   }
 
-  if (closeList.has({ x, y })) {
-    return false
+  if (containGrid(openList, x, y)) {
+    return false;
   }
 
-  return true
+  if (containGrid(closeList, x, y)) {
+    return false;
+  }
+
+  return true;
 }
 
-/**
- * @H G + B
- * @G 距离起点的步数
- * @B 不考虑障碍，到终点的最短距离（步数）
- */
+function containGrid(gridList, x, y) {
+  for (let grid of gridList) {
+    if (grid.x === x && grid.y === y) {
+      return true;
+    }
+  }
+  return false;
+}
 
 class Grid {
-  constructor(start, end, grid) {
-    this.start = start
-    this.end = end
-    this.grid = grid
+  x;
+  y;
+  f;
+  g;
+  h;
+  parent;
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 
-  initGrid() {
-    const grid = this.grid
-    if (grid.parent) {
-      grid.g = grid.parent.g + 1
+  initGrid(parent, end) {
+    this.parent = parent;
+    if (parent) {
+      this.g = parent.g + 1;
     } else {
-      grid.g = 1
+      this.g = 1;
     }
 
-    grid.b = Math.abs(this.end.y - grid.y) + Math.abs(this.end.x - grid.x)
-    grid.h = grid.g + grid.b
-    this.grid = grid
+    this.h = Math.abs(end.y - this.y) + Math.abs(end.x - this.x);
+    this.f = this.g + this.h;
   }
+}
+
+// example
+
+function example() {
+  const start = new Grid(2, 1);
+  const end = new Grid(7, 4);
+  let resultGrid = aSearch(start, end);
+  const path = new Set();
+  while (resultGrid) {
+    path.add(new Grid(resultGrid.x, resultGrid.y));
+    resultGrid = resultGrid.parent;
+  }
+
+  let str = "";
+  for (let y = 0; y < MAZE.length; y++) {
+    if (y > 0) {
+      str += "/n";
+    }
+    for (let x = 0; x < MAZE[0].length; x++) {
+      if (containGrid(path, x, y)) {
+        str += "*";
+      } else {
+        str += MAZE[x][y] + "";
+      }
+    }
+  }
+  console.log("example /n", str);
 }
